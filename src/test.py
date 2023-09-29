41daa0d564ca604e369e02c7d1664ec07f48490e
moo3030 <mohamedabdelhamid3030@gmail.com>
2023-09-29 14:12:45 +0300

    test

--- src/test.py
+++ src/test.py
@@ -6,3 +6,19 @@
 import autosklearn.pipeline.components.feature_preprocessing
 preprocessors = list(autosklearn.pipeline.components.feature_preprocessing._preprocessors.keys())
 print(list(preprocessors))
+
+
+{
+  "seed_value": 123,
+  "task_time": 300,
+  "model_time": 30,
+  "include": {
+    "regressor": [
+      "random_forest",
+      "extra_trees",
+      "gradient_boosting",
+      "k_nearest_neighbors",
+      "sgd"
+    ]
+  }
+}
